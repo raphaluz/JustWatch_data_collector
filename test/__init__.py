6ef3dcdc2159d8bcb237730284c6f783f0b8966f
Raphael Luz <59450945+raphaluz@users.noreply.github.com>
2024-09-21 01:05:26 -0400

    test: init test

---
+++ test/__init__.py
@@ -0,0 +1,9 @@
+# This file can be left empty
+
+# Alternatively, you could use it to set up test configurations,
+# import test utilities, or define test suites.
+
+# For example:
+# from .test_scraper import TestScraper
+
+# __all__ = ['TestScraper']
